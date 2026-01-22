e9baecd9893247616e5c81baf5a1a604813a9560
marwanahemm <marwan.ahemmane@gmail.com>
2026-01-22 11:18:59 +0100

    adding the Pokémon information section

---
+++ script.js
@@ -0,0 +1,11 @@
+// ========================================
+// VARIABLES GLOBALES// VARIABLES GLOBALES
+// ========================================
+
+let allPokemon = [];
+let currentIndex = 0;
+
+// Éléments DOM
+const pokemonImage = document.getElementById('pokemon-image');
+const pokemonName = document.getElementById('pokemon-name');
+const pokemonNumber = document.getElementById('pokemon-number');
